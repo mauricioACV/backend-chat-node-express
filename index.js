9f84a46bd81a7da5b470864c030edf0c18739bff
mauricioACV <mauricio.campusano@gmail.com>
2022-02-16 21:27:10 -0300

    configuracion express

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import { router } from "./router.js";
 const app = express();
 
 const httpServer = createServer(app);
-const io = new Server(httpServer{
+const io = new Server(httpServer, {
   cors: {
     origin: "https://netlify-thinks-mauricio-is-great-59894.netlify.app/"
   }
