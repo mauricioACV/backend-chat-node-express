53e08ba45b95bafc85a4ced8baac23d65d729492
mauricioACV <mauricio.campusano@gmail.com>
2022-02-16 21:17:39 -0300

    configuracion express

--- router.js
+++ router.js
@@ -2,5 +2,5 @@ import express from 'express';
 export const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('server is up and running');
+    res.send('new server is up and running');
 });
